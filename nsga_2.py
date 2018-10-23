147dfe147cbe3053676bd38f052ff25178fdca5a
maisuiqianxun <2609237930@qq.com>
2018-10-23 15:39:48 +0800

    the second versionon

--- nsga_2.py
+++ nsga_2.py
@@ -1,29 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 11 16:06:39 2018
-
 @author: maisuiY
 @algorithm:NSGA-2
 @environment:windows 10 anaconda spyder
+
 """
+
 import numpy as np
-
-
 def f_x1(x1):
     return x1*x1
-
-
 def f_x2(x2):
     return (x2-2)*(x2-2)
-
-
-def fast_non_dominated_sort(P, N, l, rank_p,length_Fy,R_decode):   # N records half of the all population of merged_Population R
-    S_p = np.zeros([2*N,2*N])-1 # each column represents a initial Sp (p=0,...,399), which includes 400 units at most to save the location of q that p dominates.
-    S_p = S_p.astype(int)
-    length_Sp=np.zeros( 2*N )
-    length_Sp=length_Sp.astype(int)
-    F_y=np.zeros([2*N,2*N])-1   #each column represents a initial set Fy (y=0,...,399(at most)), which includes 400 units at most to save the location of y that belong to the same rank.
-    F_y=F_y.astype(int)
+def fast_non_dominated_sort(N, l, rank_p,length_Fy,R_decode):   # N records half of the all population of merged_Population R
+    S_p = np.zeros([2*N,2*N],int)-1 # each column represents a initial Sp (p=0,...,399), which includes 400 units at most to save the location of q that p dominates.
+    length_Sp=np.zeros( 2*N,int )
+    F_y=np.zeros([2*N,2*N],int)-1   #each column represents a initial set Fy (y=0,...,399(at most)), which includes 400 units at most to save the location of y that belong to the same rank.
     Number_p=np.zeros( 2*N )
     k=0
     for i in range(2*N):
@@ -33,7 +25,7 @@ def fast_non_dominated_sort(P, N, l, rank_p,length_Fy,R_decode):   # N records h
             if(  (f_x1(R_decode[i]) < f_x1(R_decode[j]) and f_x2(R_decode[i]) < f_x2(R_decode[j]) ) or
                  (f_x1(R_decode[i]) < f_x1(R_decode[j])and f_x2(R_decode[i]) <= f_x2(R_decode[j]) ) or
                  (f_x1(R_decode[i]) <= f_x1(R_decode[j]) and f_x2(R_decode[i]) < f_x2(R_decode[j]) )  ):
-                S_p[ int(length_Sp[i]),i ]=j
+                S_p[length_Sp[i],i ]=j
                 length_Sp[i]+=1
             elif( (f_x1(R_decode[i]) > f_x1(R_decode[j]) and f_x2(R_decode[i]) > f_x2(R_decode[j]) ) or
                  (f_x1(R_decode[i]) > f_x1(R_decode[j])and f_x2(R_decode[i]) >= f_x2(R_decode[j]) ) or
@@ -46,7 +38,7 @@ def fast_non_dominated_sort(P, N, l, rank_p,length_Fy,R_decode):   # N records h
             k+=1
     length_Fy[0] = k
     i = 0
-    while(np.sum(F_y[:,i])  >-400 ):
+    while(np.sum(F_y[:,i])  >-2*N ):
         Q=np.zeros(2*N)-1
         k=0  #clear k for next using
         for p in F_y[:,i]:
@@ -58,6 +50,10 @@ def fast_non_dominated_sort(P, N, l, rank_p,length_Fy,R_decode):   # N records h
                             rank_p[ q ]=i+1
                             Q[k]=q
                             k+=1
+                    else:
+                        break
+            else:
+                break  
         i+=1
         length_Fy[i]=k
         F_y[:,i]=Q
@@ -68,19 +64,19 @@ def crowd_distance_assignment( L,num_solu,R_decode):
     distance_L[:,0]=L[0:num_solu]
     distance_L[:,1]=f_x1(R_decode[ L[0:num_solu] ].reshape(num_solu))
     distance_L[:,2]=f_x2(R_decode[ L[0:num_solu] ].reshape(num_solu))
-    for i in range(1,3):
+    for i in range(1,3):   # for each objective m
         f_max=np.max(distance_L[:,i])
         f_min=np.min(distance_L[:,i])
         distance_L = distance_L[distance_L[:,i].argsort()]
         distance_L[0,i]=99999999
         distance_L[num_solu-1,i]=99999999
-        for j in range(num_solu-3):
-            distance_L[j+1,i]=(distance_L[j+2,i]-distance_L[j,i])/(f_max-f_min)
+        for j in range(1,num_solu-1):
+            distance_L[j,i]=(distance_L[j+1,i]-distance_L[j-1,i])/(f_max-f_min)
     distance_final=np.zeros([num_solu,2])
     distance_final=np.reshape(distance_final,[num_solu,2])
     distance_final[:,0]=distance_L[:,0]
     distance_final[:,1]=np.sum( distance_L,1)-distance_L[:,0]
-    distance_final =distance_final[distance_final[:,1].argsort()] #sort the set L by the crowded_comparison operator<ascending>
+    distance_final =distance_final[distance_final[:,1].argsort()] # sort the set L by the crowded_comparison operator < ascending >
     return distance_final
 def make_new_pop(Parent):
     Offspring=np.array(Parent)
@@ -120,7 +116,7 @@ for genera in range(generation):
     for i in range( l ):
         R_decode=R[:,i].reshape(2*N,1)*np.power(2,i)+R_decode         #remind to clear the variables
     R_decode=-1000+R_decode*2000/(np.power(2,30)-1)
-    F=fast_non_dominated_sort(R,N,l,rank_p,length_Fy,R_decode)
+    F=fast_non_dominated_sort(N,l,rank_p,length_Fy,R_decode)
     sum_first2i=0
     for i in range(2*N):  # the flag to record the location of F_y
         sum_first2i=length_Fy[i]+sum_first2i
@@ -136,12 +132,11 @@ for genera in range(generation):
         j+=1
     if( k<N ):
         Len=crowd_distance_assignment( F[:,j],length_Fy[j],R_decode )
-        guide_Parent_new[k:N-1]=Len[length_Fy[j]-(N-k) ,0]
+        guide_Parent_new[k:N-1]=Len[length_Fy[j]-(N-k) ,0]  # copy elements from end to begin because of ascending "Len"
     guide_Parent_new=guide_Parent_new.reshape(200)
-    Parent_new=R[guide_Parent_new,:]
-    Parent=Parent_new
-    Offspring=make_new_pop(Parent_new)
-    print("the generation is %d\n th",genera )
+    Parent=R[guide_Parent_new,:]
+    Offspring=make_new_pop(Parent)
+    print("the generation is %d th\n",genera )
 
 
 
